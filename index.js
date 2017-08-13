ff28bb5ed373d2df1cd4fb6e957079cdb9616a03
Gregor <gregor@martynus.net>
2017-08-13 11:13:48 -0700

    fix: adapt for new `conventional-changelog`

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const changelog = require('conventional-changelog')
-const parseUrl = require('github-url-from-git')
+'use strict'
 
-module.exports = function (pluginConfig, options, cb) {
-  const repository = options.pkg.repository ? parseUrl(options.pkg.repository.url) : null
+const conventionalChangelog = require('conventional-changelog')
 
-  changelog({
-    version: options.pkg.version,
-    repository: repository,
-    file: false
-  }, cb)
+// pluginConfig are passed to all semantic-release plugins but are not used
+// optios have been used in an earlier version but are currently not used.
+// We leave them in as we expect to need them again in future and try to
+// avoid breaking changes
+module.exports = function (pluginConfig, options, callback) {
+  if (typeof options === 'function') {
+    callback = options
+  }
+
+  let content = ''
+  conventionalChangelog({
+    preset: 'angular'
+  })
+    .on('error', callback)
+    .on('data', (buffer) => {
+      content += buffer.toString()
+    })
+    .on('end', () => {
+      callback(null, content.trim())
+    })
 }
